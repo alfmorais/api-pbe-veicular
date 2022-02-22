47c72a80e90bf0e67595f19a382095262c4858b5
alfmorais <alfredneto@hotmail.com>
2022-02-22 20:17:23 -0300

    organizando pasta de teste

--- tests/src/test_initial.py
+++
@@ -1,7 +0,0 @@
-def test_initial():
-    first_number = 5
-    second_number = 6
-
-    assert (first_number + second_number) == 11
-    assert (first_number - second_number) == -1
-    assert (first_number * second_number) == 30
