bd8a4d55416be1aec45b8b2f253aa0f6c4ec25f3
alfmorais <alfredneto@hotmail.com>
2022-02-22 20:46:37 -0300

    configurando o swagger

--- src/pbe/settings.py
+++ src/pbe/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # libs
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
